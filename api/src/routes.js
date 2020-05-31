2a93d44fad355c68cb4e3743697e71e6c1ce95b1
Ryan Drumond <ryandrumond@outlook.com.br>
2020-05-31 12:13:07 -0300

    Retirando codigos indesejados

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,13 +2,8 @@ import { Router } from 'express';
 
 // importacao de controllers
 
+
 const routes = new Router();
 
-// criação das rotas
-routes.get('/teste', (req, res) => {
-  return res.json({
-    message: 'Matheus Nicoli is gay'
-  });
-});
 
 export default routes;
